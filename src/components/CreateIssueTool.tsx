2a6f618e4c2f9131266028352a59c334a8d5a80c
danieln-juno <danieln@junojourney.com>
2022-11-29 09:47:34 +0200

    form and routing
    added some form outline and basic routings

--- src/components/CreateIssueTool.tsx
+++ src/components/CreateIssueTool.tsx
@@ -1,5 +1,9 @@
 import { Button } from '@material-ui/core'
+import { Stack } from '@mui/material'
+import { Box } from '@mui/system'
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Form from './Form'
 
 // we will need first to be able to take a screenshot,
 //then to be able to transfer thart screenshot to editor
@@ -8,15 +12,19 @@ import React from 'react'
 
 type Props = {}
 function CreateIssueTool({ }: Props) {
+
   return (
-    <>
-      <h1>Create Issue Tool</h1>
-
-
-      <Button>cancel</Button>
-      <Button>approve</Button>
-
-    </>
+    <Stack sx={{width: "100%", height: "90vh"}}>
+      <h1 style={{ alignSelf: "center", letterSpacing: "0.2rem " }}>Create Issue Tool</h1>
+      <Stack direction="row" spacing={18}>
+        <Form />
+        <Box style={{ backgroundColor: "gray", minHeight: "20rem", maxHeight: "20rem", maxWidth: "20rem" }}>
+          HERE WE WILL HAVE A SCREENSHOT CAPTURE ?
+        </Box>
+      </Stack>
+      <Button style={{ width: "fit-content", alignSelf: "center", alignItems: "bottom" }}>cancel</Button>
+      <Button style={{ width: "fit-content", alignSelf: "center", alignItems: "bottom" }}>submit</Button>
+    </Stack>
   )
 }
 
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,20 +1,34 @@
-import { Typography } from '@mui/material'
+import { TextField, Typography } from '@mui/material'
 import { Stack } from '@mui/system'
+import { useForm } from "react-hook-form";
 import React from 'react'
+import { Outlet } from 'react-router-dom';
 
-type formValues = {
-    company: string,
-    severity: string,
-    email: string,
-    expected_behaviour: string,
-    treating_squad: string,
-    domain: string
-}
+// type formValues = {
+//     company: string,
+//     severity: string,
+//     email: string,
+//     expected_behaviour: string,
+//     treating_squad: string,
+//     domain: string
+// }
 
 const Form = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
   return (
-    <Stack>
-        <Typography>Testing</Typography>
+    <Stack sx={{ alignItems: "flex-start", width: "fit-content" }}>
+      {/* <form onSubmit={handleSubmit((data) => {
+        console.log(data)
+      })}> */}
+        <Stack direction="column" spacing={2} sx={{}}>
+          <TextField sx={{width: "fit-content"}} defaultValue="company's name" label="company name" {...register("company")} />
+          <TextField sx={{width: "fit-content"}} defaultValue="contact's full name" label="contact's full name" {...register("company")} />
+          <TextField sx={{width: "fit-content"}} defaultValue="contact's email" label="contact's email" {...register("company")} />
+          <TextField sx={{width: "fit-content"}} defaultValue="domain" label="domain" {...register("company")} />
+          <TextField sx={{width: "fit-content"}} defaultValue="company's name" label="company name" {...register("company")} />
+        </Stack>
+      {/* </form> */}
     </Stack>
   )
 }
