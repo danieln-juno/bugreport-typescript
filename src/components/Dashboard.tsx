3461247db59d35e0054f476ad77e321b0657145d
danieln-juno <danieln@junojourney.com>
2022-12-01 16:14:36 +0200

    added date captured in form

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,13 +1,15 @@
 import { Button } from '@mui/material'
 import { Stack } from '@mui/system'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 type Props = {}
 
 function Dashboard({ }: Props) {
+  const navigate = useNavigate();
   return (
     <Stack>
-      <Button variant="contained" style={{ width: "fit-content" }}>
+      <Button onClick={() => navigate('/')} variant="contained" style={{ width: "fit-content" }}>
         back
       </Button>
 
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,4 @@
-import { Button, TextField, Typography } from '@mui/material'
+import { Button, FormControl, TextField, Typography } from '@mui/material'
 import { Stack } from '@mui/system'
 import { useForm } from "react-hook-form";
 import React from 'react'
@@ -7,22 +7,29 @@ import { Box } from '@material-ui/core';
 
 const Form = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
+  const date = new Date();
+  let currentDate = String(date.getDate()) + "/" + String(date.getMonth()) + "/" + String(date.getFullYear()) + " " + date.getHours() + ":" + String(date.getMinutes()) ;
+  // {console.log(currentDate)}
   return (
     <Stack flexWrap="wrap" sx={{ alignItems: "flex-start", width: "fit-content" }}>
-      <form onSubmit={handleSubmit((data) => {
+      <FormControl onSubmit={handleSubmit((data) => {
         console.log(data)
       })}>
+
+      
         <Stack direction="column" spacing={2} sx={{}}>
-          <TextField required={true} sx={{ width: "fit-content" }} defaultValue="cmpny" label="company's name" {...register("company_name")} />
+          <TextField required={true} sx={{ width: "fit-content" }} defaultValue="" label="company's name" {...register("company_name")} />
           <TextField required={true} sx={{ width: "fit-content" }} defaultValue="" label="contact's full name" {...register("contact_fullname")} />
           <TextField required={true} sx={{ width: "fit-content" }} defaultValue="" label="contact's email" {...register("contact_email")} />
           <TextField required={true} sx={{ width: "fit-content" }} defaultValue="" label="domain" {...register("domain")} />
-        </Stack>
 
+          {/* <TextField required={true} sx={{ width: "fit-content" }} value={currentDate} disabled={true} defaultValue={currentDate} label="date captured" {...register("captured")} /> */}
+          <TextField required={true} sx={{ width: "fit-content" }} value={currentDate} disabled={true} label="date captured" {...register("captured")} />
+        
+        </Stack>
+        </FormControl>
         <Button style={{ width: "fit-content", alignSelf: "center", alignItems: "bottom", marginTop: "1rem" }}>Cancel</Button>
         <Button type='submit' style={{ width: "fit-content", alignSelf: "center", alignItems: "bottom", marginTop: "1rem" }}>Submit</Button>
-
-      </form>
 
     </Stack>
   )
