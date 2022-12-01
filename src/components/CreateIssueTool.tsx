5feb845e6dbd42223c228ed5d548037726a48307
Nir Avni <nir@junojourney.com>
2022-12-01 19:21:36 +0200

    extention

--- src/components/CreateIssueTool.tsx
+++ src/components/CreateIssueTool.tsx
@@ -2,26 +2,25 @@ import { Button } from '@material-ui/core'
 import { Stack } from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
-import { Outlet } from 'react-router-dom'
 import Form from './Form'
 import Links from './Links'
 
-// we will need first to be able to take a screenshot,
-//then to be able to transfer thart screenshot to editor
-//in the end, being able to submit and progress with the procedure, 
-//or cancel the procedure.
 
 type Props = {}
 function CreateIssueTool({ }: Props) {
 
   return (
     <Stack sx={{width: "100%", height: "90vh", alignItems: "center"}} spacing={2}>
-      <h1 style={{ alignSelf: "center", letterSpacing: "0.2rem " }}>Create Issue Tool</h1>
+      <h1 style={{ alignSelf: "center", letterSpacing: "0.2rem " }}>Issue Report</h1>
       <Stack direction="row" spacing={4}>
         <Form />
+        <Stack direction="column" spacing={4}>
         <Box style={{ backgroundColor: "gray", minHeight: "20rem", maxHeight: "20rem", maxWidth: "20rem" }}>
           HERE WE WILL HAVE A SCREENSHOT CAPTURE ?
         </Box>
+      
+        <Button style={{ width: "fit-content", alignSelf: "center"}}>Take Screen Shot</Button>
+        </Stack>
       </Stack>
       <Links/>
     </Stack>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,27 +1,30 @@
 import { Button, TextField, Typography } from '@mui/material'
 import { Stack } from '@mui/system'
-import { useForm } from "react-hook-form";
+import { Control, useForm, useFieldArray} from "react-hook-form";
 import React from 'react'
 import { Outlet } from 'react-router-dom';
 import { Box } from '@material-ui/core';
 
+
 const Form = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const { register, control, handleSubmit, reset, watch, formState: { errors }} = useForm();
+  const { fields, append, remove } = useFieldArray({ name: "issues", control });
+
   return (
     <Stack flexWrap="wrap" sx={{ alignItems: "flex-start", width: "fit-content" }}>
       <form onSubmit={handleSubmit((data) => {
-        console.log(data)
-      })}>
-        <Stack direction="column" spacing={2} sx={{}}>
-          <TextField required={true} sx={{ width: "fit-content" }} defaultValue="cmpny" label="company's name" {...register("company_name")} />
-          <TextField required={true} sx={{ width: "fit-content" }} defaultValue="" label="contact's full name" {...register("contact_fullname")} />
-          <TextField required={true} sx={{ width: "fit-content" }} defaultValue="" label="contact's email" {...register("contact_email")} />
-          <TextField required={true} sx={{ width: "fit-content" }} defaultValue="" label="domain" {...register("domain")} />
+          append({ issue: { issue_title: data.issue_title, description: data.description } })
+          console.log(data);
+        })}
+      >
+        <Stack direction="column" spacing={2} sx={{width: 300}}>
+          <TextField  defaultValue="" label="Issue Title" {...register("issue_title")} />
+          <TextField  multiline rows={10} defaultValue="" label="Description" {...register("description")} />
         </Stack>
 
         <Button style={{ width: "fit-content", alignSelf: "center", alignItems: "bottom", marginTop: "1rem" }}>Cancel</Button>
         <Button type='submit' style={{ width: "fit-content", alignSelf: "center", alignItems: "bottom", marginTop: "1rem" }}>Submit</Button>
-
       </form>
 
     </Stack>
