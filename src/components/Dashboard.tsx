e757ea58edda4e8bcf5dfd0699e146c2561fcf10
danieln-juno <danieln@junojourney.com>
2022-12-01 15:46:04 +0200

    added back button on pages

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,10 +1,17 @@
+import { Button } from '@mui/material'
+import { Stack } from '@mui/system'
 import React from 'react'
 
 type Props = {}
 
-function Dashboard({}: Props) {
+function Dashboard({ }: Props) {
   return (
-    <div>Dashboard</div>
+    <Stack>
+      <Button variant="contained" style={{ width: "fit-content" }}>
+        back
+      </Button>
+
+    </Stack>
   )
 }
 
